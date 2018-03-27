59d6a2b21db65991c0602e2eb9d7c63c9888266f
Clarisa <Clarisa002@github.com>
2018-03-26 17:43:00 -0700

    final

---
+++ starter_code/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+$('#city-type').click(function(){
+event.preventDefault();
+});
+
+var cities = ["NYC", "SF", "LA", "ATX", "SYD"];
+
+for(var i = 0; i < cities.length; i++){
+	$("#city-type").append('<option>' + cities[i] + '</option>');
+};
+
+function cityImg(){
+var city = $('#city-type').val();
+
+
+
+if (city === 'NYC') {
+$('body').attr('class','nyc')
+		};
+		if (city === 'SF') {
+$('body').attr('class','sf')
+		};
+if (city === 'LA') {
+$('body').attr('class','la')
+		};
+		if (city === 'ATX') {
+$('body').attr('class','atx')
+		};
+		if (city === 'SYD') {
+$('body').attr('class','syd')
+		};
+	}
+		$('#city-type').change(cityImg);
+
+});
+
